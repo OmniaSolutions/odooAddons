db4c0aad98f7c76fdc11e89ee0b11151be529084
daniel <daniel@win7-td-13.station>
2015-07-17 12:23:07 +0200

    commit before merge

--- addons/timesheet_rfid/test/test.py
+++ addons/timesheet_rfid/test/test.py
@@ -41,48 +41,53 @@ HOURS_LIST = [
 
 
 
-                #OmniaSolutions
-                #Test chiusura alle 12
-            (117, timeNow.replace( day=7, month=7, hour=9, minute=0)), 
-            (117, timeNow.replace( day=8, month=7, hour=10, minute=0)), 
-                   
-                #Test chiusura alle 18
-            (117, timeNow.replace( day=8, month=7, hour=12, minute=0)), 
-            (117, timeNow.replace( day=8, month=7, hour=13, minute=0)), 
-            (117, timeNow.replace( day=9, month=7, hour=10, minute=5)), 
-                   
-                #Test minuti non recuperati la sera
-            (117, timeNow.replace( day=9, month=7, hour=12, minute=0)), 
-            (117, timeNow.replace( day=9, month=7, hour=13, minute=0)),
-            (117, timeNow.replace( day=9, month=7, hour=17, minute=1)),
-                   
-                #Test minuti recuperati la sera
-            (117, timeNow.replace( day=10, month=7, hour=8, minute=5)),
-            (117, timeNow.replace( day=10, month=7, hour=12, minute=0)), 
-            (117, timeNow.replace( day=10, month=7, hour=13, minute=0)),
-            (117, timeNow.replace( day=10, month=7, hour=17, minute=6)),
-                   
-                #Test entro alle 17:00 ed entro alle 17:30        a mezzogiorno e mezzo perdo la mezz'ora
-            (117, timeNow.replace( day=11, month=7, hour=8, minute=0)),
-            (117, timeNow.replace( day=11, month=7, hour=12, minute=29)),
-            (117, timeNow.replace( day=11, month=7, hour=17, minute=0)),
-            (117, timeNow.replace( day=11, month=7, hour=17, minute=30)),
-    
-                #Test esco 12:30 entro 13:00 e perdo la mezz'ora perche' ho un'ora di pausa pranzo
-            (117, timeNow.replace( day=12, month=7, hour=7, minute=48)),
-            (117, timeNow.replace( day=12, month=7, hour=12, minute=33)),
-            (117, timeNow.replace( day=12, month=7, hour=13, minute=0)),
-            (117, timeNow.replace( day=12, month=7, hour=17, minute=00)),
-              
-                #Test entro alle 17:30 e non esco
-            (117, timeNow.replace( day=13, month=7, hour=8, minute=0)),
-            (117, timeNow.replace( day=13, month=7, hour=12, minute=0)),
-            (117, timeNow.replace( day=13, month=7, hour=18, minute=30)),
-            (117, timeNow.replace( day=14, month=7, hour=8, minute=05)),
-  
-                #Test entro alle 16:30 ed esco alle 17:01 non recuperando il ritardo della mattina
-            (117, timeNow.replace( day=14, month=7, hour=12, minute=29)),
-            (117, timeNow.replace( day=14, month=7, hour=16, minute=30)),
-            (117, timeNow.replace( day=14, month=7, hour=17, minute=01)),
+#                 #OmniaSolutions
+#                 #Test chiusura alle 12
+#             (117, timeNow.replace( day=7, month=7, hour=9, minute=0)), 
+#             (117, timeNow.replace( day=8, month=7, hour=10, minute=0)), 
+#                    
+#                 #Test chiusura alle 18
+#             (117, timeNow.replace( day=8, month=7, hour=12, minute=0)), 
+#             (117, timeNow.replace( day=8, month=7, hour=13, minute=0)), 
+#             (117, timeNow.replace( day=9, month=7, hour=10, minute=5)), 
+#                    
+#                 #Test minuti non recuperati la sera
+#             (117, timeNow.replace( day=9, month=7, hour=12, minute=0)), 
+#             (117, timeNow.replace( day=9, month=7, hour=13, minute=0)),
+#             (117, timeNow.replace( day=9, month=7, hour=17, minute=1)),
+#                    
+#                 #Test minuti recuperati la sera
+#             (117, timeNow.replace( day=10, month=7, hour=8, minute=5)),
+#             (117, timeNow.replace( day=10, month=7, hour=12, minute=0)), 
+#             (117, timeNow.replace( day=10, month=7, hour=13, minute=0)),
+#             (117, timeNow.replace( day=10, month=7, hour=17, minute=6)),
+#                    
+#                 #Test entro alle 17:00 ed entro alle 17:30        a mezzogiorno e mezzo perdo la mezz'ora
+#             (117, timeNow.replace( day=11, month=7, hour=8, minute=0)),
+#             (117, timeNow.replace( day=11, month=7, hour=12, minute=29)),
+#             (117, timeNow.replace( day=11, month=7, hour=17, minute=0)),
+#             (117, timeNow.replace( day=11, month=7, hour=17, minute=30)),
+#     
+#                 #Test esco 12:30 entro 13:00 e perdo la mezz'ora perche' ho un'ora di pausa pranzo
+#             (117, timeNow.replace( day=12, month=7, hour=7, minute=48)),
+#             (117, timeNow.replace( day=12, month=7, hour=12, minute=33)),
+#             (117, timeNow.replace( day=12, month=7, hour=13, minute=0)),
+#             (117, timeNow.replace( day=12, month=7, hour=17, minute=00)),
+#               
+#                 #Test entro alle 17:30 e non esco
+#             (117, timeNow.replace( day=13, month=7, hour=8, minute=0)),
+#             (117, timeNow.replace( day=13, month=7, hour=12, minute=0)),
+#             (117, timeNow.replace( day=13, month=7, hour=18, minute=30)),
+#             (117, timeNow.replace( day=14, month=7, hour=8, minute=05)),
+#   
+#                 #Test entro alle 16:30 ed esco alle 17:01 non recuperando il ritardo della mattina
+#             (117, timeNow.replace( day=14, month=7, hour=12, minute=29)),
+#             (117, timeNow.replace( day=14, month=7, hour=16, minute=30)),
+#             (117, timeNow.replace( day=14, month=7, hour=17, minute=01)),
 
+              #Toffoli
+              (38, timeNow.replace( day=9, month=7, hour=12, minute=01)),
+              (38, timeNow.replace( day=9, month=7, hour=13, minute=33)),
+              (38, timeNow.replace( day=9, month=7, hour=17, minute=59)),
+              (38, timeNow.replace( day=13, month=7, hour=16, minute=24)),
               ]
