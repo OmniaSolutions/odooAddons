16c039aceba2b5a14ade608e0e2ca03261b1fb69
mboscolo <matteo.boscolo@omniasolutions.eu>
2018-06-28 16:18:01 +0200

    FIX: singleton error

--- addons/sale_order_line_date_request/models/sale_order.py
+++ addons/sale_order_line_date_request/models/sale_order.py
@@ -18,16 +18,17 @@ class SaleOrder(models.Model):
     @api.multi
     def _is_out_of_request_date(self):
         outMsg = []
-        for line in self.order_line:
-            if line.lineIsOutRequestDate():
-                outMsg.append("""<div style="background-color:red;font-size: 20px;color:white">Product <b>[%r]</b> in late</div><br/>""" % line.name)
-        htmlBody = ""
-        found = False
-        for line in outMsg:
-            htmlBody += line
-            found = True
-        if found:
-            self.is_out_of_request_date = htmlBody
+        for order in self:
+            for line in order.order_line:
+                if line.lineIsOutRequestDate():
+                    outMsg.append("""<div style="background-color:red;font-size: 20px;color:white">Product <b>[%r]</b> in late</div><br/>""" % line.name)
+            htmlBody = ""
+            found = False
+            for line in outMsg:
+                htmlBody += line
+                found = True
+            if found:
+                order.is_out_of_request_date = htmlBody
     is_out_of_request_date = fields.Html(string="Check if the move line request date is different from expected date",
                                          compute=_is_out_of_request_date)
 
