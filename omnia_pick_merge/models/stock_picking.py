9d1831dc4632377dd0e9dd8988078cf49f0df75b
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2019-06-05 12:55:08 +0200

    FIX:	- Raise message if wrong procedure

--- omnia_pick_merge/models/stock_picking.py
+++ omnia_pick_merge/models/stock_picking.py
@@ -19,20 +19,26 @@ class stock_picking_custom(models.Model):
 
     @api.multi
     def action_cancel(self):
-        for pickBrws in self:
-            if pickBrws.state == 'cancel':
+        msg = ''
+        for merged_pick in self:
+            if merged_pick.state == 'cancel':
                 continue
-            for move_line in pickBrws.move_lines:
+            for move_line in merged_pick.move_lines:
                 old_move = self.env['stock.move'].search([('id', '=', move_line.from_move_id)])
                 if old_move:
-                    old_move.product_uom_qty = old_move.product_uom_qty + move_line.product_uom_qty
-                    old_move.state = 'draft'
-            pickBrws.mapped('move_lines').action_cancel()
-            pickBrws.write({'is_locked': True})
-            for oldPick in pickBrws.merged_pick_ids:
-                oldPick.state = 'draft'
-        return super(stock_picking_custom, self).action_cancel()
-    
+                    if old_move.state in ['draft', 'waiting', 'confirmed', 'cancel'] and old_move.picking_id.state in ['draft', 'waiting', 'confirmed', 'cancel']:
+                        old_move.product_uom_qty = old_move.product_uom_qty + move_line.product_uom_qty
+                        old_move.state = 'draft'
+                    else:
+                        msg += "\nOriginal picking %r has not been restored due to state" % (old_move.picking_id.display_name)
+            merged_pick.mapped('move_lines').action_cancel()
+            merged_pick.write({'is_locked': True})
+        res = super(stock_picking_custom, self).action_cancel()
+        self.env.cr.commit()
+        if msg:
+            raise UserError(msg)
+        return res
+
     @api.multi
     def unlink(self):
         for pickBrws in self:
