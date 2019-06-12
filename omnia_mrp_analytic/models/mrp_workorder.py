4ab806fda9a54922ff5b15ed33781a605d8147f8
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2019-06-12 17:21:40 +0200

    FIX	: - Create timesheet in hours instead of minutes

--- omnia_mrp_analytic/models/mrp_workorder.py
+++ omnia_mrp_analytic/models/mrp_workorder.py
@@ -58,6 +58,6 @@ class MrpWorkorder(models.Model):
                         'project_id': self.task_id.project_id.id,
                         'task_id': self.task_id.id,
                         'employee_id': user_id.employee_ids.id,
-                        'unit_amount': task_duration,
+                        'unit_amount': task_duration / 60,
                         }
                 self.env['account.analytic.line'].create(vals)
