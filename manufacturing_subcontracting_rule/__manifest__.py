004ee503dc531d4ff53590e9bfe2cc0d804affd6
Matteo Boscolo <matteo.oscolo@omniasolutions.eu>
2019-08-10 11:12:50 +0200

    FIX: problem on ir.values

--- manufacturing_subcontracting_rule/__manifest__.py
+++ manufacturing_subcontracting_rule/__manifest__.py
@@ -49,7 +49,7 @@
              'views/purchase_extension.xml',
              'views/mrp_bom.xml',
              'views/res_partner.xml',
-             'views/mrp_workorder.xml',
+             #'views/mrp_workorder.xml',
     ],
     'installable': True,
     'application': False,
