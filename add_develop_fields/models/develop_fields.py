99f59dd0ae3c1911073f5cc3982d6ab8560b4c57
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2021-09-24 15:51:02 +0200

    fix mistake

--- add_develop_fields/models/develop_fields.py
+++ add_develop_fields/models/develop_fields.py
@@ -19,7 +19,7 @@ class Base(models.AbstractModel):
     @api.model
     def fields_view_get(self, view_id=None, view_type='form', toolbar=False, submenu=False):
         ret = super(Base, self).fields_view_get(view_id=view_id, view_type=view_type, toolbar=toolbar, submenu=submenu)
-        if self.env.user.asGroup('base.group_no_one'):
+        if self.env.user.has_group('base.group_no_one'):
             try:
                 if view_type == 'tree':
                     arch = ret.get('arch', '')
