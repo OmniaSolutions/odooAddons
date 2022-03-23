4e4428ca4c8957ef3e60c4e5bc3e72c900e860f0
Daniel Smerghetto <daniel.smerghetto@omniasolutions.eu>
2022-03-23 16:54:35 +0100

    IMP:	- Migrated omnia_replannable_workorder

---
+++ omnia_replannable_workorder/models/stock_move.py
@@ -0,0 +1,47 @@
+# -*- encoding: utf-8 -*-
+##############################################################################
+#
+#    OmniaSolutions, Open Source Management Solution
+#    Copyright (C) 2010-2011 OmniaSolutions (<http://www.omniasolutions.eu>). All Rights Reserved
+#    $Id$
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+'''
+Created on May 16, 2018
+
+@author: daniel
+'''
+
+from odoo import models
+from odoo import fields
+from odoo import api
+from odoo import _
+
+
+class StockMoveExtension(models.Model):
+    _name = 'stock.move'
+    _inherit = 'stock.move'
+
+    def open_related_workorder(self):
+        self.ensure_one()
+        if self.workorder_id:
+            return {
+                "type": "ir.actions.act_window",
+                "res_model": "mrp.workorder",
+                "views": [[False, "form"]],
+                "res_id": self.workorder_id.id,
+            }
---
+++ omnia_replannable_workorder/wizard/wizard.py
@@ -0,0 +1,61 @@
+'''
+Created on 16 Jan 2018
+
+@author: mboscolo
+'''
+from odoo.addons import decimal_precision as dp
+from odoo import models
+from odoo import fields
+from odoo import api
+from odoo import _
+import logging
+import datetime
+from dateutil.relativedelta import relativedelta
+
+
+class ChangeProductMoveWizard(models.TransientModel):
+    _name = "change_product_move_wizard"
+
+    stock_moves = fields.Many2many('stock.move')
+    product_id = fields.Many2one('product.product',
+                                 'New Product')
+    warning_message = fields.Html('Warning Message', compute="_product_to_replace")
+    
+    @api.depends('stock_moves', 'product_id')
+    def _product_to_replace(self):
+        for wizardBrws in self:
+            wizardBrws.warning_message = ''
+            outMsg = '<div style="background-color: #ffc97a;">Following raw materials will be replaced:<br></br>'
+            products = []
+            for move in wizardBrws.stock_moves:
+                if move.state not in ['cancelled', 'done']:
+                    prodId = move.product_id.id
+                    if prodId not in products:
+                        products.append(prodId)
+                        outMsg += '[%s] %s<br></br>' % (move.workorder_id.production_id.display_name, move.product_id.display_name)
+            outMsg += '</div>'
+            if len(products) > 1:
+                wizardBrws.warning_message = outMsg
+
+    def changeProduct(self):
+        for move in self.stock_moves:
+            if move.state not in ['cancelled', 'done']:
+                move.product_id = self.product_id
+            move.workorder_id.get_raw_material()
+        
+    def ShowWizard(self, ids):
+        new_me = self.create({})
+        workorder_ids = self.env['mrp.workorder'].search([('id', 'in', ids)])
+        for workorder_id in workorder_ids:
+            for move_id in workorder_id.production_id.move_raw_ids:
+                if move_id.state not in ['done', 'cancelled']:
+                    new_me.stock_moves = [(4, move_id.id)]
+
+        action = {'name': 'Change Raw product',
+                  'view_type': 'form',
+                  'view_mode': 'form',
+                  'target': 'new',
+                  'res_id': new_me.id,
+                  'res_model': 'change_product_move_wizard',
+                  'type': 'ir.actions.act_window'}
+        return action
