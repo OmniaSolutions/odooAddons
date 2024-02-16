51e7dc1dd81ca1dc3dff595e82d4354a3caa29c2
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-02-16 08:21:47 +0100

    IMP: version number

--- omnia_workorder_machine/__manifest__.py
+++ omnia_workorder_machine/__manifest__.py
@@ -21,7 +21,7 @@
 ##############################################################################
 {
     'name': 'Omnia Workorder Machine',
-    'version': '14.0',
+    'version': '14.1',
     'author': 'OmniaSolutions',
     'website': 'http://www.omniasolutions.eu',
     'category': 'report',
