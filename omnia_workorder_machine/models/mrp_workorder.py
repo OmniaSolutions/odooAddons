5007120d2ade9ccd6ad7ec23a29f763b6a9cd3a7
Matteo Boscolo <matteo.boscolo@omniasolutions.eu>
2024-10-14 15:57:15 +0200

    IMP: ordering of workorder

--- omnia_workorder_machine/models/mrp_workorder.py
+++ omnia_workorder_machine/models/mrp_workorder.py
@@ -86,7 +86,7 @@ class MrpProductionWCLine(models.Model):
             return []
         searchFilter = [('state', 'in', ['ready', 'progress'])]
         logging.info('Getting Work Orders with search %r' % (searchFilter))
-        woBrwsList = self.search(searchFilter, order='date_planned_start ASC,id ASC')
+        woBrwsList = self.search(searchFilter, order='tag_ids ASC, date_planned_start ASC,id ASC')
         woBrwsList = woBrwsList.filtered(lambda x: user_id in x.user_ids.ids or x.user_id.id == user_id)
         if not user_id:
             user_id = self.getUserId()
@@ -102,7 +102,7 @@ class MrpProductionWCLine(models.Model):
             return []
         searchFilter = [('state', 'in', ['ready', 'progress'])]
         logging.info('Getting Work Orders with search %r' % (searchFilter))
-        woBrwsList = self.search(searchFilter, order='date_planned_start ASC,id ASC')
+        woBrwsList = self.search(searchFilter, order='tag_ids ASC, date_planned_start ASC,id ASC')
         woBrwsList = woBrwsList.filtered(lambda x: employee_id in x.employee_ids.ids)
         user_id = self.getUserId()
         out = self.getDictWorkorder(woBrwsList.with_user(user_id))
@@ -114,7 +114,7 @@ class MrpProductionWCLine(models.Model):
     def getWorkordersByDomain(self, domain=[], listify=False):
         domain.append(('state', 'in', ['ready', 'progress']))
         woBrwsList = self.search(domain,
-                                 order='date_planned_start ASC,id ASC')
+                                 order='tag_ids ASC, date_planned_start ASC,id ASC')
         user_id = self.getUserId()
         out = self.getDictWorkorder(woBrwsList.with_user(user_id))
         if listify:
